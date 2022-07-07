d3fde67e23fff20b637f6cd0dfe73303a410ae86
unknown <muriloue@gmail.com>
2022-07-07 14:29:39 -0300

    Heroku setup

--- server.js
+++ server.js
@@ -1,11 +1,8 @@
 const express = require("express");
-const { resove } = require("path")
 
 const app = express()
 
-app.use("/", express.static(
-    resolve(__dirname, "./dist"),
-))
+app.use("/", express.static("./dist"))
 
 app.listen(proccess.env.PORT || 3000, (err) => {
     if(err) { return console.log(err)}
