e0829a915b0562ac2a41f77dab87c781d4975d7e
Murilo Santos <muriloue@gmail.com>
2022-07-07 20:51:48 -0300

    Heroku setup

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,8 +1,7 @@
 const config = {
   passwordRegex: /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/,
   emailRegex: /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/,
-  // @ts-ignore
-  baseUrl: process.env.API_URL,
+  baseUrl: "https://vanilla-ecommerce-backend.herokuapp.com/",
 };
 
 export default config;
