7fcc7ff9ea2ef5797084275145226cbc517de3cc
Murilo Santos <muriloue@gmail.com>
2022-07-07 16:51:01 -0300

    Heroku setup

---
+++ server.js
@@ -0,0 +1,18 @@
+const dotenv = require("dotenv");
+const express = require("express");
+const path = require('path');
+
+dotenv.config()
+const app = express()
+const distPath = path.join(__dirname, "..", "dist")
+const port = process.env.VITE_APP_API_URL || 3000
+
+app.use(express.static(distPath));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(distPath, 'index.html'))
+})
+
+app.listen(port, () => {
+    console.log("Running")
+})
