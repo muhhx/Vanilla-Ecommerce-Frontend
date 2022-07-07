c9fe0344d3c168a9e86f27e17b1b807675a71223
Murilo Santos <muriloue@gmail.com>
2022-07-07 20:45:37 -0300

    Heroku setup

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,7 +1,8 @@
 const config = {
   passwordRegex: /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/,
   emailRegex: /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/,
-  baseUrl: import.meta.env.VITE_APP_API_URL,
+  // @ts-ignore
+  baseUrl: process.env.API_URL,
 };
 
 export default config;
