047c2c8a96d92956ef65dd943a7eb8d103812770
unknown <muriloue@gmail.com>
2022-07-07 14:31:17 -0300

    Heroku setup

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express()
 
 app.use("/", express.static("./dist"))
 
-app.listen(proccess.env.PORT || 3000, (err) => {
+app.listen(import.meta.env.VITE_APP_PORT || 3000, (err) => {
     if(err) { return console.log(err)}
 
     console.log("Running...")
