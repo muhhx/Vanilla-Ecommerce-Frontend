e51205d980b7c81c0ce3ef3d7bc48438d1e9c5d7
Murilo Santos <muriloue@gmail.com>
2022-07-11 16:39:32 -0300

    Stopped tracking .env File

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,7 +1,7 @@
 const config = {
   passwordRegex: /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/,
   emailRegex: /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/,
-  baseUrl: import.meta.env.VITE_APP_API_URL || "http://localhost:4000",
+  baseUrl: "https://vanilla-ecommerce-backend.herokuapp.com/",
 };
 
 export default config;
